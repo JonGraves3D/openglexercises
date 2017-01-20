f19c12369dce9ee3ba11420ec3e799c23066c26c
jongraves3d <graves.jon@gmail.com>
2017-01-19 18:10:14 -0800

    cleaned up glm::vec and glm::mat

--- Transform.cpp
+++ Transform.cpp
@@ -10,10 +10,12 @@ using glm::vec3;
 using glm::mat3;
 using glm::mat4;
 
+
+
 mat3 Transform::rotate(const float degrees, const vec3& axis) {
   // YOUR CODE FOR HW1 HERE
 
-	glm:mat3 m1 = glm::mat3(
+	glm:mat3 m1 = mat3(
 		axis.x*axis.x, axis.x*axis.y, axis.x*axis.z,
 		axis.x*axis.y, axis.y*axis.y, axis.y*axis.z,
 		axis.x*axis.y, axis.y*axis.z, axis.z*axis.z
@@ -21,37 +23,33 @@ mat3 Transform::rotate(const float degrees, const vec3& axis) {
 	
 	m1 = glm::transpose(m1);
 
-	glm::mat3 m2 = glm::mat3(
+	mat3 m2 = mat3(
 		0, -axis.z, axis.y, 
 		axis.z, 0, -axis.x, 
 		-axis.y, axis.x, 0);
 
 	m2 = glm::transpose(m2);
 
-	glm::mat3 rotation = cos(degrees * pi / 180.0f) * glm::mat3(1.0f) + (1 - cos(degrees * pi / 180.0f)) * m1 + sin(degrees * pi / 180.0f) * m2;
+	mat3 rotation = cos(degrees * pi / 180.0f) * mat3(1.0f) + (1 - cos(degrees * pi / 180.0f)) * m1 + sin(degrees * pi / 180.0f) * m2;
 
   // You will change this return call
   return rotation;
 }
 
 
-glm::vec3 cross(glm::vec3 v1, glm::vec3 v2)
+vec3 cross(vec3 v1, vec3 v2)
 {
 
 	float a = v1.y * v2.z - v1.z * v2.y;
 	float b = v1.z * v2.x - v1.x * v2.z;
 	float c = v1.x * v2.y - v1.y * v2.x;
 
-	printf("a:%.2f  \n", a);
-	printf("b:%.2f  \n", b);
-	printf("c:%.2f  \n", c);
-
-	glm::vec3 crossproduct = glm::vec3(a, b, c);
+	vec3 crossproduct = vec3(a, b, c);
 
 	return crossproduct;
 }
 
-glm::vec3 normalize(glm::vec3 vector)
+vec3 normalize(vec3 vector)
 {
 	float w = sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
 
@@ -74,7 +72,7 @@ void Transform::left(float degrees, vec3& eye, vec3& up) {
 // Transforms the camera up around the "crystal ball" interface
 void Transform::up(float degrees, vec3& eye, vec3& up) {
 
-	glm::vec3 axis = cross(eye, up);
+	vec3 axis = cross(eye, up);
 	axis = normalize(axis);
 	up = Transform::rotate(degrees, axis) * up;
 	eye = Transform::rotate(degrees, axis) * eye;
@@ -85,17 +83,17 @@ void Transform::up(float degrees, vec3& eye, vec3& up) {
 // Your implementation of the glm::lookAt matrix
 mat4 Transform::lookAt(vec3 eye, vec3 up) {
 
-	glm::vec3 w = normalize(eye);
+	vec3 w = normalize(eye);
 	up = normalize(up);
-	glm::vec3 u = cross(up, w);
-	glm::vec3 v = normalize(cross(w,u));
+	vec3 u = cross(up, w);
+	vec3 v = normalize(cross(w,u));
 
 //	glm::mat4 rotation = glm::mat4(
 //		u.x, u.y, u.z, 0,
 //		v.x, v.y, v.z, 0,
 //		w.x, w.y, w.z, 0,
 //		  0,   0,   0, 1);
-	glm::mat4 rotation = glm::mat4(
+	mat4 rotation = mat4(
 		u.x, v.x, w.x, 0, 
 		u.y, v.y, w.y, 0, 
 		u.z, v.z, w.z, 0,
@@ -103,7 +101,7 @@ mat4 Transform::lookAt(vec3 eye, vec3 up) {
 
 //	rotation = glm::transpose(rotation);
 
-	glm::mat4 translation = glm::mat4(
+	mat4 translation = mat4(
 		1, 0, 0, -eye.x,
 		0, 1, 0, -eye.y,
 		0, 0, 1, -eye.z,
@@ -111,7 +109,7 @@ mat4 Transform::lookAt(vec3 eye, vec3 up) {
 
 	translation = glm::transpose(translation);
 
-	glm::mat4 transformation = rotation * translation;
+	mat4 transformation = rotation * translation;
 
 	return transformation;
 
